d74fdbcb132ce13cf1e0d17ff2c388407ffeb5c9
QuanNg1996 <quanguyen250@gmail.com>
2020-05-15 17:31:34 -0400

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Layout>
-          <BurgerBuilder />
-        </Layout>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Layout>
+              <Route path="/checkout" component={Checkout} />
+              <Route exact path="/" component={BurgerBuilder} />
+            </Layout>
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
