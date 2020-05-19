afe5602fbcc81d9ef830efae1d22787ce83990a0
QuanNg1996 <quanguyen250@gmail.com>
2020-05-19 13:35:09 -0400

    fetch orders

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,7 +4,7 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem link="/">Burger Builder</NavigationItem>
+    <NavigationItem exact link="/">Burger Builder</NavigationItem>
     <NavigationItem link="/orders">Orders</NavigationItem>
   </ul>
 );
