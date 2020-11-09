9db926d55095a86de5fbbb38ea66c77f23ccf112
QuanNg1996 <quanguyen250@gmail.com>
2020-11-09 00:15:14 -0500

    redux

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import ContactData from './ContactData/ContactData';
@@ -17,17 +17,25 @@ class Checkout extends Component {
   }
 
   render() {
+    let summary = <Redirect to="/" />
+    if (this.props.ings) {
+      summary = (
+      <div>
+          <CheckoutSummary
+            ingredients={this.props.ings}
+            checkoutCancelled={this.checkoutCancelledHandler}
+            checkoutContinued={this.checkoutContinuedHandler}
+          />
+          <Route
+            path={this.props.match.path + '/contact-data'}
+            component={ContactData}
+          />
+      </div>
+      );
+    }
     return (
       <div>
-        <CheckoutSummary
-          ingredients={this.props.ings}
-           checkoutCancelled={this.checkoutCancelledHandler}
-           checkoutContinued={this.checkoutContinuedHandler}
-          />
-        <Route
-          path={this.props.match.path + '/contact-data'}
-          component={ContactData}
-        />
+        {summary}
       </div>
     );
   }
@@ -35,7 +43,7 @@ class Checkout extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    ings: state.ingredients,
+    ings: state.burgerBuilder.ingredients,
   }
 };
 
